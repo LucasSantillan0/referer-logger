143cb87bdd513f381768dc9386a3cb617b2f8d82
Lucas <lusantil@pichincha.com>
2023-08-18 16:50:29 -0300

    changing to post

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 3000;
 
-app.get("/", (req, res) => {
+app.post("/", (req, res) => {
   const referer = req.headers.referer || req.headers.referrer;
   console.log("Referer:", referer);
   res.send("Referer logged");
