3da8fde7ea40bbfa45c3a33aef647fc3d6fd6731
Lucas <lusantil@pichincha.com>
2023-08-18 17:13:47 -0300

    changing funciton directory

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,8 +4,22 @@ import serverless from "serverless-http";
 const api = express();
 
 const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
+
 
 api.use("/api/", router);
+
+const logs = [];
+const log = (a) => {
+  logs.push(a);
+};
+
+router.post("/", (req, res) => {
+  const referer = req.headers.referer || req.headers.referrer;
+  log("Referer:" + referer);
+  res.send("Referer logged");
+});
+router.get("/log", (req, res) => {
+  res.json({ logs });
+});
 
 export const handler = serverless(api);
